0b0966cd9ff85e63c104ec721ad583bfcd927c2a
Nishinandan <hmnishi33@gmail.com>
2026-09-19 23:48:40 +0530

    Q5

---
+++ src/Week4/Assignments/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,45 @@
+package Week4.Assignments;
+
+import java.util.Scanner;
+
+public class FindMinimumInRotatedSortedArray {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        System.out.println("Enter the rotated sorted elements:");
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+
+                // Minimum is on the right side
+                left = mid + 1;
+
+            } else {
+
+                // Minimum is at mid or on the left side
+                right = mid;
+            }
+        }
+
+        System.out.println("Minimum Element: " + nums[left]);
+
+        sc.close();
+    }
+}
