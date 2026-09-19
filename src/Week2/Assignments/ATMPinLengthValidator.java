83c22050bb9c3182e368831a30ff14ea81c1f999
Nishinandan <hmnishi33@gmail.com>
2026-09-19 23:23:07 +0530

    Q1

---
+++ src/Week2/Assignments/ATMPinLengthValidator.java
@@ -0,0 +1,25 @@
+package Week2.Assignments;
+
+import java.util.Scanner;
+
+public class ATMPinLengthValidator {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter PIN: ");
+        String pin = sc.nextLine();
+
+        if (pin.length() != 4) {
+
+            System.out.println("Invalid PIN — must be exactly 4 digits.");
+
+        } else {
+
+            System.out.println("PIN length OK.");
+        }
+
+        sc.close();
+    }
+}
