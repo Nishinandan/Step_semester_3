bc0ff033f2666f12b33e1f8763d0fd383ff5e080
Nishinandan <hmnishi33@gmail.com>
2026-09-20 17:40:48 +0530

    Score card

---
+++ src/Week7/Scorecard.java
@@ -0,0 +1,28 @@
+package Week7;
+
+public class Scorecard {
+    private final boolean[] results;
+    private int count;
+
+    public Scorecard(int totalQuestions) {
+        this.results = new boolean[totalQuestions];
+        this.count = 0;
+    }
+
+    public void recordAnswer(boolean isCorrect) {
+        if (count < results.length) {
+            results[count] = isCorrect;
+            count++;
+        }
+    }
+
+    public int getScore() {
+        int score = 0;
+        for (int i = 0; i < count; i++) {
+            if (results[i]) {
+                score++;
+            }
+        }
+        return score;
+    }
+}
