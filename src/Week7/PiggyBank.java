94ddc46becd6381d43505790f114b1a3855cbe38
Nishinandan <hmnishi33@gmail.com>
2026-09-20 17:38:15 +0530

    PIGGY BANK

---
+++ src/Week7/PiggyBank.java
@@ -0,0 +1,32 @@
+package Week7;
+public class PiggyBank {
+    private final String id;
+    private double savings;
+
+    public PiggyBank(String id) {
+        this.id = id;
+        this.savings = 0.0;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            savings += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= savings) {
+            savings -= amount;
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+
+    public double getSavings() {
+        return savings;
+    }
+
+    public String getId() {
+        return id;
+    }
+}
